ede49b0dbae23eb4e11a322542010b344a356420
aaditbhatia06-ship-it <aaditbhatia06@gmail.com>
2026-07-11 23:56:33 +0530

    Time: 23 ms (17.93%), Space: 19.3 MB (29.48%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,40 +1,23 @@
-# class Solution:
-#     def lengthOfLongestSubstring(self, s: str) -> int:
-#         f = {}
-#         res = 0
-#         n = len(s)
-#         l = 0
-#         for h in range(n):
-#             if s[h] not in f:
-#                 f[s[h]]=0
-#             f[s[h]]+=1
-            
-#             k = h-l+1
-#             while len(f)<k:
-#                 f[s[l]] -=1
-#                 if(f[s[l]]==0):
-#                     del f[s[l]]
-#                 l += 1
-#                 k = h-l+1
-#             if len(f)==k:
-#                 le = h-l+1
-#                 res = max(res,le)
-#         return res
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        freq = {}
+        f = {}
+        res = 0
+        n = len(s)
         l = 0
-        ans = 0
-
-        for r in range(len(s)):
-            freq[s[r]] = freq.get(s[r], 0) + 1
-
-            while freq[s[r]] > 1:
-                freq[s[l]] -= 1
-                if freq[s[l]] == 0:
-                    del freq[s[l]]
+        for h in range(n):
+            if s[h] not in f:
+                f[s[h]]=0
+            f[s[h]]+=1
+            
+            k = h-l+1
+            while len(f)<k:
+                f[s[l]] -=1
+                if(f[s[l]]==0):
+                    del f[s[l]]
                 l += 1
-
-            ans = max(ans, r - l + 1)
-
-        return ans
+                k = h-l+1
+            if len(f)==k:
+                le = h-l+1
+                res = max(res,le)
+        return res
+        
