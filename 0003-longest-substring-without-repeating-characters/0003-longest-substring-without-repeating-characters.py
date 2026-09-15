88e3b44ab91408b5cc50b09cd7ffdb4c6c38359a
aaditbhatia06-ship-it <aaditbhatia06@gmail.com>
2026-09-15 10:46:42 +0530

    Time: 371 ms (10.4%), Space: 19.9 MB (58.71%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,42 +1,42 @@
-# class Solution:
-#     def lengthOfLongestSubstring(self, s: str) -> int:
-#         f = {}
-#         res = 0
-#         n = len(s)
-#         l = 0
-#         for h in range(n):
-#             if s[h] not in f:
-#                 f[s[h]]=0
-#             f[s[h]]+=1
-            
-#             k = h-l+1
-#             while len(f)<k:
-#                 f[s[l]] -=1
-#                 if(f[s[l]]==0):
-#                     del f[s[l]]
-#                 l += 1
-#                 k = h-l+1
-#             if len(f)==k:
-#                 le = h-l+1
-#                 res = max(res,le)
-#         return res
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        freq = {}
+        f = {}
+        res = 0
+        n = len(s)
         l = 0
-        ans = 0
-
-        for r in range(len(s)):
-            if s[r] not in freq:
-                freq[s[r]]=0
-            freq[s[r]]+=1
-
-            while freq[s[r]] > 1:
-                freq[s[l]] -= 1
-                if freq[s[l]] == 0:
-                    del freq[s[l]]
+        for h in range(n):
+            if s[h] not in f:
+                f[s[h]]=0
+            f[s[h]]+=1
+            
+            k = h-l+1
+            while len(f)<k:
+                f[s[l]] -=1
+                if(f[s[l]]==0):
+                    del f[s[l]]
                 l += 1
+                k = h-l+1
+            if len(f)==k:
+                le = h-l+1
+                res = max(res,le)
+        return res
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         freq = {}
+#         l = 0
+#         ans = 0
 
-            ans = max(ans, r - l + 1)
+#         for r in range(len(s)):
+#             if s[r] not in freq:
+#                 freq[s[r]]=0
+#             freq[s[r]]+=1
 
-        return ans
+#             while freq[s[r]] > 1:
+#                 freq[s[l]] -= 1
+#                 if freq[s[l]] == 0:
+#                     del freq[s[l]]
+#                 l += 1
+
+#             ans = max(ans, r - l + 1)
+
+#         return ans
